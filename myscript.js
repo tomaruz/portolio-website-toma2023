ef8b32258c5c6211abb9f4b7d14f74e4f6bc469d
tomaruz <115160722+tomaruz@users.noreply.github.com>
2023-05-02 17:17:25 +0100

    footer

--- myscript.js
+++ myscript.js
@@ -1,15 +1,5 @@
 console.log('hello')
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
