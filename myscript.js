abf191556fc05e4145cdbaba9c920576d033b6cd
tomaruz <115160722+tomaruz@users.noreply.github.com>
2023-05-04 15:42:25 +0100

    fadein

--- myscript.js
+++ myscript.js
@@ -10,3 +10,17 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  // GALLERY FADE IN ANIMATION
+
+  // add the fadeInTop class to project-container when it comes into view 
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry =>{
+  if(entry.isIntersecting){
+    document.querySelectorAll(".animated")[0].classList.add("fadeInTop")
+  }
+})
+})
+
+observer.observe(document.querySelector(".project-container"));
