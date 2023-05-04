6e8d6a38029369747ee8ee8a0c66d8ce4694912c
tomaruz <tomaruz2002@gmail.com>
2023-05-05 00:41:55 +0100

    nochanges

--- myscript.js
+++ myscript.js
@@ -1,6 +1,5 @@
 console.log('hello')
 
-
   // GALLERY FADE IN ANIMATION
 
   const observer = new IntersectionObserver((entries, observer) => {
@@ -9,11 +8,11 @@ console.log('hello')
         setTimeout(() => {
           entry.target.classList.add("show");
           observer.unobserve(entry.target); //stop observing the element once it's visible, so animation runs only once
-        }, 100); // add 0.2 second delay before fading in the element
+        }, 100); // add 0.1 second delay before fading in the element
       }
     });
   }, { 
-    threshold: 0.5, // fade in when element is half up the screen
+    threshold: 0.4, // fade in when element is 40% up the screen
     once: true 
   });
   
