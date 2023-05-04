864512000e55bf20fe870024d34b30ab6dc0091a
tomaruz <tomaruz2002@gmail.com>
2023-05-05 00:33:21 +0100

    menu underline done

--- myscript.js
+++ myscript.js
@@ -1,46 +1,21 @@
 console.log('hello')
 
 
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
   // GALLERY FADE IN ANIMATION
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-        observer.unobserve(entry.target); // stop observing the element once it's visible, so animation runs only once
+        setTimeout(() => {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); //stop observing the element once it's visible, so animation runs only once
+        }, 100); // add 0.2 second delay before fading in the element
       }
     });
-  }, { once: true });
+  }, { 
+    threshold: 0.5, // fade in when element is half up the screen
+    once: true 
+  });
   
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
-  
-
-
-
-  // using GREENSOCK
-
-  // gsap.utils.toArray('.project-container').forEach(box => {
-  //   gsap.fromTo(box, {
-  //       autoAlpha: 0, 
-  //       y: 50
-  //     }, {
-  //     scrollTrigger: {
-  //       trigger: box,
-  //       once: true,
-  //     },
-  //     duration: 2, 
-  //     autoAlpha: 1, 
-  //     y: 0
-  //   });
-  // });
